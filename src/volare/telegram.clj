8d242f21349f15a29a3f4de2b9a1ee43f05193cb
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2025-02-12 14:18:19 +0100

    fixup! Add lein and deps

--- src/volare/telegram.clj
+++ src/volare/telegram.clj
@@ -1,6 +1,5 @@
 (ns volare.telegram
-  (:require [telegrambot-lib.core :as tbot])
-  (:require '[environ.boot :refer [env]]))
+  (:require [telegrambot-lib.core :as tbot]
+            [environ.core :refer [env]]))
 
-(def bot-api-token
-  (env :bot-api-token))
+(def bot-api-token (env :bot-api-token))
